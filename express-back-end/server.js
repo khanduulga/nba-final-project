2b9eefb9393dcd810b3d7dddeb8c53e0986c9c8b
deonleer <deon.leer96@gmail.com>
2021-02-13 22:41:28 +0000

    started building api, added curry and lebron shots/stats

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -2,6 +2,10 @@ const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
+const lebron_shots = require('./data/lebron_shots.json')
+const curry_shots = require('./data/curry_shots.json')
+const lebron_stats = require('./data/lebron_stats.json')
+const curry_shots = require('./data/curry_stats.json')
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
@@ -9,10 +13,20 @@ App.use(BodyParser.json());
 App.use(Express.static('public'));
 
 // Sample GET route
-App.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
-
+App.get('/api/shots', (req, res) => 
+  {if (req.query.name === "lebron") {
+    res.json(lebron_shots)
+  } else if (req.query.name === "curry") {
+    res.json(curry_shots)
+  }}
+)
+App.get('/api/stats', (req, res) =>
+  {if (req.query.name === "lebron") {
+    res.json(lebron_stats)
+  } else if (req.query.name === "curry") {
+    res.json(curry_stats)
+  }}
+)
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
