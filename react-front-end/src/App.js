19304550b5aae04db18d69d530bce0927b71a691
Colin Elsinga <colinelsinga@Colins-MBP-2.router.taotronics.com>
2021-02-14 00:33:24 -0800

    implement and style navbar

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import PersistentDrawerRight from './components/navbar'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   constructor(props) {
@@ -26,10 +29,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
+        <PersistentDrawerRight />
+        <h1>Find a Player</h1>
+        <div class="search">
+          <div style={{marginRight: 1 + 'em'}}>
+          <TextField
+              label="Search"
+              variant="outlined"
+
+            />
+          </div>
+          <Button variant="contained" style={{backgroundColor: '#311b92', color: 'white'}}  href="">
+          Select
+        </Button>  
+       </div>      
       </div>
     );
   }
