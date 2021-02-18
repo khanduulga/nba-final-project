fd1372202042c4c4509a38a60e596de5b33cdf31
Colin Elsinga <colinelsinga@Colins-MBP-2.router.taotronics.com>
2021-02-18 11:13:56 -0800

    made game scores horizontally scrollable

--- react-front-end/src/components/gameScores.js
+++ react-front-end/src/components/gameScores.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { useEffect, useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 
 import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    overflow: 'auto',
+    overflowX: 'auto',
     padding: theme.spacing(0, 1),
     marginTop: -6,
-    height: 110
+    height: 110,
+    display: 'flex',
+    width: '100vw'
   },
   paper: {
-    width: '14em',
-    margin: `${theme.spacing(1)}px auto`,
+    minWidth: '14em',
+    margin: `${theme.spacing(1)}px ${theme.spacing(1)}px`,
     padding: theme.spacing(2),
     backgroundColor: '#949499',
     color: 'white',
@@ -29,7 +30,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paperAlt: {
     width: 100,
-    margin: `${theme.spacing(1)}px auto`,
+    margin: `${theme.spacing(1)}px ${theme.spacing(1)}px`,
     padding: theme.spacing(1),
     backgroundColor: '#242429',
     color: 'white',
@@ -72,20 +73,20 @@ export default function GameScores() {
 
   const mappedGames = currentGames.map(game => {
     return (
-      <Grid item xs={1.5}>
+      // <Grid item xs={1.5}>
         <Paper className={classes.paper}>
           <img className={classes.img} alt="awayLogo" src={`/team-logos/${game.awayTeam.teamTricode}.svg`} /> {game.awayTeam.teamTricode} {game.period > 0 ? game.awayTeam.score : null} {game.period > 0 ? null : "at"} <img className={classes.img} alt="homeLogo" src={`/team-logos/${game.homeTeam.teamTricode}.svg`} />{game.homeTeam.teamTricode} {game.period > 0 ? game.homeTeam.score : null} {game.gameStatusText}
         </Paper>
-      </Grid>
+      // </Grid>
     )
   })
 
   function FormRow() {
     return (
       <React.Fragment>
-        <Grid item xs={1.5}>
+        {/* <Grid item xs={1.5}> */}
           <Paper className={classes.paperAlt}>Scores</Paper>
-        </Grid>
+        {/* </Grid> */}
         {mappedGames}
       </React.Fragment>
     );
@@ -93,11 +94,11 @@ export default function GameScores() {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={1}>
-        <Grid container item xs={12} spacing={1}>
+      {/* <Grid container spacing={1}> */}
+        {/* <Grid container item xs={12} spacing={1}> */}
           <FormRow />
-        </Grid>
-      </Grid>
+        {/* </Grid> */}
+      {/* </Grid> */}
     </div>
   );
 }
